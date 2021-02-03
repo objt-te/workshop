6969c04aebf8063c961c450f7f9e3f56258ddaa1
timeeckhaut <tim.eeckhaut@objt.com>
2021-02-03 14:36:53 +0100

    provide api for hello person

---
+++ main/src/HelloPerson.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * Copyright (c) 2020 Objective
+ * All rights reserved
+ *
+ * This software is the confidential and proprietary information of Objective.
+ * You shall not disclose this confidential information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with
+ * Objective.
+ *******************************************************************************/
+
+public class HelloPerson
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello " + getName());
+  }
+
+  private static String getName()
+  {
+    return "demo name";
+  }
+}
